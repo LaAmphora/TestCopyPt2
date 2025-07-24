9c548a38c7286443fead4562ead21fcd23ace322
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-24 11:59:59 -0400

    Update copy.py

--- copy.py
+++ copy.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 text_to_copy = st.text_input("Hello, World!")
 
-hosted_html_file = "https://testcopy.streamlit.app/files/copy.html"
+hosted_html_file = "https://testcopy.streamlit.app"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
 st.markdown(f'<iframe style="overflow: hidden;" src="{iframe_url}"></iframe>', unsafe_allow_html=True)
