e44571e31cd214d0ff0505065df206e7fb64616a
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-24 12:12:22 -0400

    Update copy.py

--- copy.py
+++ copy.py
@@ -6,4 +6,22 @@ text_to_copy = st.text_input("Hello, World!")
 hosted_html_file = "https://testcopy.streamlit.app/files/copy.html"
 iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
 
-st.markdown(f'<iframe style="overflow: hidden;" src="{iframe_url}"></iframe>', unsafe_allow_html=True)
+st.markdown(f"""
+    <input type="text" value="{text_to_copy}" id="copyInput" style="position: absolute; left: -9999px;">
+    <button id="copyButton" onclick="copyText()" 
+        style="font-size: 20px; background: none; border: none; cursor: pointer;">📋</button>
+
+    <script>
+    function copyText() {{
+        var copyInput = document.getElementById("copyInput");
+        copyInput.select();
+        document.execCommand("copy");
+
+        var btn = document.getElementById("copyButton");
+        btn.innerHTML = "✔";
+        setTimeout(function() {{
+            btn.innerHTML = "📋";
+        }}, 1000);
+    }}
+    </script>
+""", unsafe_allow_html=True)
