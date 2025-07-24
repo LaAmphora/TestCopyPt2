fd9b6e27ad95a7ada9fcaea7ebf85a15aec1bfb2
Gabriella Ramirez <114959659+LaAmphora@users.noreply.github.com>
2025-07-24 11:54:55 -0400

    First Commit

---
+++ copy.py
@@ -0,0 +1,9 @@
+###copy.py
+import streamlit as st
+
+text_to_copy = st.text_input("Hello, World!")
+
+hosted_html_file = "https://everydayswag.org/files/copy.html"
+iframe_url = f"{hosted_html_file}?copy={text_to_copy}"
+
+st.markdown(f'<iframe style="overflow: hidden;" src="{iframe_url}"></iframe>', unsafe_allow_html=True)
